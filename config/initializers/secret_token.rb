2b76cc97e5e67285b6e3a3ec160e15493e7351e6
Jinxy05 <julianhurley11@gmail.com>
2014-03-02 00:46:09 +0000

    install bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BootstrapIntro::Application.config.secret_key_base = 'be70efa85b97c9fed347b1dbb030cb1b2b0f8b019aec37dece3e6c79e07df0319a838923a7c506f471c284999acdccff71941de1ebe00aed5fa154b2b9859893'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BootstrapIntro::Application.config.session_store :cookie_store, key: '_bootstrap_intro_session'
